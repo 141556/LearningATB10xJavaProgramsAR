f450647c510f181788ea8532208d31c2402b30df
Aswini <jangalaaswini9919@gmail.com>
2025-02-28 22:22:58 +0530

    initial labs

---
+++ src/Java_Basics/Lab004_MultiComment.java
@@ -0,0 +1,10 @@
+package Java_Basics;
+
+public class Lab004_MultiComment {
+    /* Author: Aswini
+     * Associate Product engineer
+     */
+    public static void main(String[] args) {
+        System.out.println("Multi Comment");
+    }
+}
