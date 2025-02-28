355954d9647c92c4fbd82cf88e2e65c2d54a7caa
Aswini <jangalaaswini9919@gmail.com>
2025-02-28 22:28:19 +0530

    initial labs

---
+++ src/Java_Basics/Lab005_println_print.java
@@ -0,0 +1,11 @@
+package Java_Basics;
+
+public class Lab005_println_print {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Good morning.......!");
+
+        System.out.print("Hi ");
+        System.out.print("Good Morning!");
+    }
+}
